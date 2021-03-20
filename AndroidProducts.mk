b4cf04c8648fde5cdb75ffdb8422ffb3b235083c
Slowhy <we@jcts.pw>
2021-03-20 20:23:23 +0800

    Revert "joan: PE-ify"
    This reverts commit 9b49cd205094c222aee57a9ea727769fa398e02e.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosp_joan.mk
+    $(LOCAL_DIR)/lineage_joan.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_joan-userdebug \
-    aosp_joan-eng
+    lineage_joan-userdebug \
+    lineage_joan-eng
