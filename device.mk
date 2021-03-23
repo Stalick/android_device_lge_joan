3726b73ff5a9297a91b6ca9a28407a99224f30dc
Slowhy <we@jcts.pw>
2021-03-23 20:47:05 +0800

    joan: Don't build textclassifier
    * Target no longer exists
    https://source.android.com/devices/tech/display/textclassifier#11-release

--- device.mk
+++ device.mk
@@ -432,10 +432,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
-
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
