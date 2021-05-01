8bb02a89efe8326643ff983196e5acba97e67e93
Slowhy <we@jcts.pw>
2021-05-01 19:13:22 +0800

    Revert "Revert "joan-common: add Pocketmode""
    This reverts commit 50d51d85d652ee1a2fa3b0c26ff4cdd3a5905f9d.

--- device.mk
+++ device.mk
@@ -179,7 +179,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1 \
-    android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.1.vendor \
+    LGEPocketMode
 
 # FM packages
 #PRODUCT_PACKAGES += \
