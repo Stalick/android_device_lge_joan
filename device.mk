d732e5d52ed1272a440c070870eacf5d9b9aedd6
Slowhy <we@jcts.pw>
2021-06-20 16:15:58 +0800

    joan: Add apns-conf.xml

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# APN
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
