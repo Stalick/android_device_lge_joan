7b251c81af18764369d070c16e186e897424b0a9
Stalick <noreply48392342@rep432.com>
2022-02-03 23:37:55 -0400

    Corvus bringup
    bringup
    
    
    Delete lineage.dependencies

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/lineage_joan.mk
+    $(LOCAL_DIR)/corvus_joan.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_joan-userdebug \
-    lineage_joan-eng
+    corvus_joan-userdebug \
+    corvus_joan-eng
